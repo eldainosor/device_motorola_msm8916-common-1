2c733026a1205cf2dd9c648b8dd8b4a4c6b4d20f
Prema Chand Alugu <premaca@gmail.com>
2017-08-23 17:05:05 +0530

    bacon: Link log library for LOGS

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -48,6 +48,8 @@ LOCAL_COPY_HEADERS:= \
     loc_core_log.h \
     LocAdapterProxyBase.h
 
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
