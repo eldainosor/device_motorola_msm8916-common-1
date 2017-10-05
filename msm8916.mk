7e21b9eec5eac75396d2ab0275019195415b0e93
Alberto97 <albertop2197@gmail.com>
2017-10-05 20:16:53 +0200

    msm8916-common: Use Lineage interface for legacy camera
    Change-Id: I716aaff0db3dd37451d4b99b06f3b295714888ed

--- msm8916.mk
+++ msm8916.mk
@@ -83,7 +83,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl.legacy \
+    camera.device@1.0-impl.legacy \
     camera.msm8916 \
     camera.device@1.0-impl \
     libbson \
